152641cab213d3dada917fd0e4b36c680862bb4a
Joe C. Antonelli <joecantonelli@gmail.com>
2016-02-14 04:14:07 -0500

    add 'htmlString' js helper for HTML element creation, start building navigation elements in the header

---
+++ javascript/application.js
@@ -0,0 +1,69 @@
+﻿//HTML elements helper - thanks to MARC GRABANSKI for his helpful example!
+  htmlString = function(tag, content, attributes) {
+
+    //some basic parameter handling
+      if(arguments.length == 0) {
+     
+        tag        =  "p";
+        content    =  "This is a test paragraph created by the htmlString javascript function.";
+        attributes =  { id: "defaultParagraph" };
+
+  	  } else if(arguments.length !== 3) {
+     
+        console.warn("The htmlString function requires three valid arguments to build HTML elements. You may alternatively use no arguments for testing purposes.");
+        return;
+      };
+
+    var element = '<' + tag;
+  
+    for(key in attributes) {
+      element += ' ' + key + '="' + attributes[key] + '"';
+    };
+
+    //some basic tag handling
+      if(tag == "link" || tag == "img") {
+
+        return element + ' />';
+
+      } else {
+
+        return element + '>' + content + '</' + tag + '>';
+      };
+  };
+
+
+
+//build the navigation elements common to all HTML pages, then build the content unique to each page
+  createHeader = function(callback) {
+  
+    //header icon
+      icon = htmlString('img', '', {
+
+                 id: "icon",
+                 alt: "Risk Cloud",
+                 src: "../images/icon.png",
+                 style: "width: 12.5%"
+               }
+             );
+
+    //navigation menu
+      var menuHeadings = { one: 'model', two: 'about' },
+          listItems = '';
+
+      for(heading in menuHeadings) {
+
+        listItems += htmlString('li', htmlString('a', menuHeadings[heading], { href: "#", class: "nav-item" }), {});
+      };
+
+      navMenu = htmlString('ul', listItems, {});
+
+      navMain = htmlString('div', icon + navMenu, { class: "nav-main" });
+  
+  
+    content = callback();
+
+    return document.write( navMain + content );
+  };
+
+
+
---
+++ javascript/home.js
@@ -0,0 +1,38 @@
+﻿//build HTML elements native to the home page
+  createHomePage = function() {
+
+    //the home page welcome message
+      risk = htmlString('span', 'risk', {
+
+                 id: "risk",
+                 style: "color: #ff5555;"
+               }
+             );
+
+      profit = htmlString('span', 'profit', {
+
+                   id: "profit",
+                   style: "color: #55bb55;"
+                 }
+               );
+
+      welcome = htmlString('h1', 'Estimate '+risk+' and '+profit+' potential in your option(s) trades.', { id: "welcome" });
+
+
+    //the home page background icon
+      icon2 = htmlString('img', '', {
+
+                  id: "icon2",
+                  alt: "Watermark Icon",
+                  src: "../images/icon2.png",
+                  style: "width: 25%;"
+                }
+              );
+
+      watermark = htmlString('div', icon2, { id: "watermark" });
+
+
+    return welcome + watermark;
+  };
+
+  
