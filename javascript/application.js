7c1adf04d1978ae2039b3de5a5c681c27e33fe03
Joe C. Antonelli <joecantonelli@gmail.com>
2016-02-16 14:15:32 -0500

    Finish header navigation menu

--- javascript/application.js
+++ javascript/application.js
@@ -1,27 +1,27 @@
 ﻿//HTML elements helper - thanks to MARC GRABANSKI for his helpful example!
   htmlString = function(tag, content, attributes) {
 
-    //some basic parameter handling
+    //SOME BASIC PARAMETER HANDLING
       if(arguments.length == 0) {
-     
-        tag        =  "p";
-        content    =  "This is a test paragraph created by the htmlString javascript function.";
+
+        tag        =  'p';
+        content    =  'This is a test paragraph created by the htmlString javascript function.';
         attributes =  { id: "defaultParagraph" };
 
   	  } else if(arguments.length !== 3) {
-     
+
         console.warn("The htmlString function requires three valid arguments to build HTML elements. You may alternatively use no arguments for testing purposes.");
         return;
       };
 
     var element = '<' + tag;
-  
+
     for(key in attributes) {
       element += ' ' + key + '="' + attributes[key] + '"';
     };
 
-    //some basic tag handling
-      if(tag == "link" || tag == "img") {
+    //SOME BASIC TAG HANDLING
+      if(tag == 'meta' || tag == 'link' || tag == 'img') {
 
         return element + ' />';
 
@@ -32,38 +32,52 @@
   };
 
 
-
-//build the navigation elements common to all HTML pages, then build the content unique to each page
+//build the header common to all HTML pages, then build the content unique to each page; use the 'htmlString' function above
   createHeader = function(callback) {
-  
-    //header icon
-      icon = htmlString('img', '', {
 
-                 id: "icon",
-                 alt: "Risk Cloud",
-                 src: "../images/icon.png",
-                 style: "width: 12.5%"
-               }
-             );
+    //HEADER ICON
+      icon = htmlString('img', '', { id: "icon", alt: "Risk Cloud", src: "../images/icon.png" });
 
-    //navigation menu
-      var menuHeadings = { one: 'model', two: 'about' },
-          listItems = '';
+    //BUILD THE NAVIGATION MENU RECURSIVELY
+      //BUILD THE SUB-MENUS
+        //MODELS SUB-MENU
+        var modelsSubHeadings = { one: 'European: &nbsp&nbsp Black-Scholes', two: 'American: &nbsp&nbsp Binomial' },
+            modelsListItems   = '';
 
-      for(heading in menuHeadings) {
+        for(num in modelsSubHeadings) {        
+          modelsListItems += htmlString('li', htmlString('a', modelsSubHeadings[num], { href: "#", class: "nav-sub-item" }), {});
+        };
 
-        listItems += htmlString('li', htmlString('a', menuHeadings[heading], { href: "#", class: "nav-item" }), {});
+        modelsSubMenu = htmlString('div', htmlString('div', htmlString('ul', modelsListItems, {}), { class: "nav-sub" }), { class: "nav-sub-container" });
+
+        //INFO SUB-MENU
+        var infoSubHeadings = { one: 'about', two: 'white papers' },
+            infoListItems   = '';
+
+        for(num in infoSubHeadings) {
+          infoListItems += htmlString('li', htmlString('a', infoSubHeadings[num], { href: "#", class: "nav-sub-item" }), {});
+        };
+
+        infoSubMenu = htmlString('div', htmlString('div', htmlString('ul', infoListItems, {}), { class: "nav-sub" }), { class: "nav-sub-container" });
+
+        //CREATE A SUB-MENUS OBJECT FOR EASY ADDITION TO THE MAIN MENU
+        var subMenus = { one: modelsSubMenu, two: infoSubMenu };
+
+      //BUILD THE MAIN MENU
+      var menuHeadings  = { one: 'models', two: 'info' },
+          menuListItems = '';
+
+      for(num in menuHeadings) {
+        menuListItems += htmlString('li', htmlString('a', menuHeadings[num], { href: "#", class: "nav-item" }) + subMenus[num], {});
       };
 
-      navMenu = htmlString('ul', listItems, {});
+      navMenu = htmlString('ul', menuListItems, {});
 
+    //BUILD THE CONTAINER FOR THE HEADER, ADD THE ICON AND THE COMPLETED NAVIGATION MENU
       navMain = htmlString('div', icon + navMenu, { class: "nav-main" });
-  
-  
-    content = callback();
+
+    //CREATE ANY CONTENT UNIQUE TO THE CURRENT PAGE
+      content = callback();
 
     return document.write( navMain + content );
   };
-
-
-
--- javascript/home.js
+++ javascript/home.js
@@ -1,38 +1,17 @@
 ﻿//build HTML elements native to the home page
   createHomePage = function() {
 
-    //the home page welcome message
-      risk = htmlString('span', 'risk', {
+    //THE HOME PAGE WELCOME MESSAGE
+      risk = htmlString('span', 'risk', { id: "risk" });
 
-                 id: "risk",
-                 style: "color: #ff5555;"
-               }
-             );
+      profit = htmlString('span', 'profit', { id: "profit" });
 
-      profit = htmlString('span', 'profit', {
+      welcome = htmlString('h1', 'Estimate '+risk+' and '+profit+' potential in option(s) trades.', { id: "welcome" });
 
-                   id: "profit",
-                   style: "color: #55bb55;"
-                 }
-               );
-
-      welcome = htmlString('h1', 'Estimate '+risk+' and '+profit+' potential in your option(s) trades.', { id: "welcome" });
-
-
-    //the home page background icon
-      icon2 = htmlString('img', '', {
-
-                  id: "icon2",
-                  alt: "Watermark Icon",
-                  src: "../images/icon2.png",
-                  style: "width: 25%;"
-                }
-              );
+    //THE HOME PAGE BACKGROUND ICON
+      icon2 = htmlString('img', '', { id: "icon2", alt: "Watermark Icon", src: "../images/icon2.png" });
 
       watermark = htmlString('div', icon2, { id: "watermark" });
 
-
     return welcome + watermark;
   };
-
-  
