bae809e65b8f2ccdbb8d72bd78b29651f36efbe3
Joe C. Antonelli <joecantonelli@gmail.com>
2016-07-01 17:24:07 -0400

    Add prototype for vertical gridlines, refactor some code

--- javascript/datavis.js
+++ javascript/datavis.js
@@ -42,16 +42,15 @@ visuals = function(properties) {
   			camera.aspect = (width/height);
   		});
 
-		//set camera position and add camera, light to the scene
+		//set camera position and add light, camera to the scene
 		camera.position.set(radius*Math.sin(phi)*Math.cos(theta), radius*Math.sin(phi)*Math.sin(theta), radius*Math.cos(phi));
-		scene.add(camera);
-		scene.add(light);
+		scene.add(light, camera);
 
 
-		//the 2D view
+		//THE 2D VIEW
 		show2DView = function() {
 
-			//local vars for graph
+			//LOCAL VARS FOR THE GRAPH
 			var //
 			 	//general and background vars
 				//
@@ -61,7 +60,7 @@ visuals = function(properties) {
 				VFOVrad = Math.PI/4,
 				zDist = -h/(2*Math.tan(VFOVrad/2)),
 				plane1 = new THREE.Mesh(new THREE.PlaneGeometry(w, h, 1, 1), new THREE.MeshBasicMaterial({ color: 'rgb(200,200,200)' })),
-				plane2 = new THREE.Mesh(new THREE.PlaneGeometry(scalar*w, scalar*h, 1, 1), new THREE.MeshBasicMaterial({ color: 0x333333 })),
+				plane2 = new THREE.Mesh(new THREE.PlaneGeometry(w*scalar, h*scalar, 1, 1), new THREE.MeshBasicMaterial({ color: 0x333333 })),
 				//
 				//gridlines and labels vars
 				//
@@ -70,24 +69,24 @@ visuals = function(properties) {
         		canvasW = 3*Math.floor(width*(1-(1.01)*scalar)),
         		canvasH = 3*Math.floor(height*(0.6)*scalar/(numHGridLines-1)),
 	        	xPos = canvasW/2,
-	        	yPos = 5*canvasH/6,
-	        	gridlines = { xaxis: {}, yaxis: {} }, //COULD WE USE ANOTHER DATA STRUCTURE HERE?
-				labels = { xaxis: {canvas: {}, context: {}, texture: {}, mesh: {}}, yaxis: {canvas: {}, context: {}, texture: {}, mesh: {}} },
+	        	yPos = 5*canvasH/6, //WHY IS THIS NOT 'canvasH/2'?
+	        	lines = { xaxis: {}, yaxis: {} }, //COULD WE USE ANOTHER DATA STRUCTURE HERE?
+				labels = { xaxis: {canvas: {}, context: {}, texture: {}, mesh: {}}, yaxis: {canvas: {}, context: {}, texture: {}, mesh: {}} }, //AND HERE?
 				//
 	   	     	//data vars
 	   	     	//
 	        	data = g.PROFITLOSS_DATA,
 	        	globalRange = Math.abs(obj.max([obj.max(data[0]), obj.max(data[obj.min(g.EXPIRY)])])-obj.min([obj.min(data[0]), obj.min(data[obj.min(g.EXPIRY)])])),
 				dataSets = { T: data[obj.min(g.EXPIRY)], 0: data[0] }, //COULD WE USE ANOTHER DATA STRUCTURE HERE?
-				cloud = { T: new THREE.Points(new THREE.Geometry(), new THREE.PointsMaterial({size: 1.75*scalar*w/obj.size(dataSets[0]), color: 0xff0000})),
-						  0: new THREE.Points(new THREE.Geometry(), new THREE.PointsMaterial({size: 1.75*scalar*w/obj.size(dataSets[0]), color: 0x0000ff})) };
+				cloud = { T: new THREE.Points(new THREE.Geometry(), new THREE.PointsMaterial({size: 1.75*w*scalar/obj.size(dataSets[0]), color: 0xff0000})),
+						  0: new THREE.Points(new THREE.Geometry(), new THREE.PointsMaterial({size: 1.75*w*scalar/obj.size(dataSets[0]), color: 0x0000ff})) };
 
-			//background
+			//position the background
 			plane1.position.set(0, 0, zDist);
 			plane2.position.set(w/2*(1-scalar), h/2*(1-scalar*numHGridLines/(numHGridLines-1)), zDist);
 			camera.add(plane1, plane2);
 
-			//add horizontal gridlines and labels
+			//ADD HORIZONTAL GRIDLINES AND LABELS
 			for(var i=0; i<numHGridLines; i++) {
 
 				//gridlines
@@ -97,9 +96,7 @@ visuals = function(properties) {
 					gridlineGeom.vertices.push(new THREE.Vector3(w/2*(1-(1.01)*scalar), h/2*(1-scalar*(2*i+1)/(numHGridLines-1)), zDist)); //center vertex
 					gridlineGeom.vertices.push(new THREE.Vector3(w/2, h/2*(1-scalar*(2*i+1)/(numHGridLines-1)), zDist)); //right vertex
 
-				gridlines.yaxis[i] = new THREE.Line(gridlineGeom, new THREE.LineBasicMaterial({color: 0x444444}));
-
-				camera.add(gridlines.yaxis[i]);
+				lines.yaxis[i] = new THREE.Line(gridlineGeom, new THREE.LineBasicMaterial({color: 0x444444}));
 
 				//labels
 				labels.yaxis.canvas[i] = document.createElement('canvas');
@@ -120,15 +117,15 @@ visuals = function(properties) {
 
 				switch(i) {
 
+					//set color and value of the x-axis
 					case (numHGridLines-1)/2:
-						//set color and value of the x-axis
-						gridlines.yaxis[i].material.color.setHex(0x777777);
+						lines.yaxis[i].material.color.setHex(0x777777);
 						labels.yaxis.context[i].fillText('0', xPos, yPos);
 						break;
 
+					//set all other gridline values
 					default:
-						//set gridline value
-						labels.yaxis.context[i].fillText(+(globalRange*(1-2*i/(numHGridLines-1))).toFixed(2), xPos, yPos);
+						labels.yaxis.context[i].fillText((globalRange*(1-2*i/(numHGridLines-1))).toFixed(2), xPos, yPos);
 				}
 
 				labels.yaxis.texture[i] = new THREE.Texture(labels.yaxis.canvas[i]);
@@ -144,17 +141,42 @@ visuals = function(properties) {
 									   );
 
 				labels.yaxis.mesh[i].position.set(-w/2*(1.01)*scalar, h/2*(1-scalar*((2*i+1+(0.6)/4)/(numHGridLines-1)-(0.002))), zDist);
-				camera.add(labels.yaxis.mesh[i]);
+
+				//add gridlines and labels to the scene
+				camera.add(lines.yaxis[i], labels.yaxis.mesh[i]);
 			}
 
-			//add vertical gridlines and labels
-			for(var i=0; i<numVGridLines; i++) {
+			//ADD VERTICAL TICK MARKS AND DOTTED LINES, LABELS
+			//for(var i=0; i<numVGridLines; i++) {
 
+				//tick marks
+				var tickGeom = new THREE.Geometry();
 
-				//MOR COD HEER PLZ
-			}
+				tickGeom.vertices.push(new THREE.Vector3(w*(-scalar+0.5), h/2*(1-scalar*2), zDist)); //top vertex
+				tickGeom.vertices.push(new THREE.Vector3(w*(-scalar+0.5), h/2*(1-scalar*(2*numHGridLines-1)/(numHGridLines-1)), zDist)); //center vertex
+				tickGeom.vertices.push(new THREE.Vector3(w*(-scalar+0.5), h/2*(1-scalar*(2*numHGridLines)/(numHGridLines-1)), zDist)); //bottom vertex
 
-			//data
+				tick = new THREE.Line(tickGeom, new THREE.LineBasicMaterial({color: 0xffff00}));
+
+				//dotted lines
+				var dotLine = new THREE.Points(new THREE.Geometry(), new THREE.PointsMaterial({size: 1.75*w*scalar/obj.size(dataSets[0]), color: 0xffff00}));
+
+				for(var j=0; j<(4*numHGridLines-6); j++) {
+				
+					dotLine.geometry.vertices.push(new THREE.Vector3(
+
+						w*(-scalar+0.5), //x-coordinate
+
+						h*(0.5+scalar*(0.25*(j+1)/(numHGridLines-1)-1)), //y-coordinate
+
+						zDist //z-coordinate
+					));
+				}
+
+				camera.add(tick, dotLine);
+			//}
+
+			//DATA
 			for(t in dataSets) {
 
 				//loop variable
@@ -165,17 +187,16 @@ visuals = function(properties) {
 
 					cloud[t].geometry.vertices.push(new THREE.Vector3(
 
-						scalar*w*((k+0.5)/obj.size(dataSets[0])-0.5), //x-coordinate
+						w*(scalar*((k+0.5)/obj.size(dataSets[0])-1)+0.5), //x-coordinate
 
-						scalar*h*(dataSets[t][val]/(2*globalRange)), //y-coordinate
+						h/2*(scalar*(dataSets[t][val]/globalRange-numHGridLines/(numHGridLines-1))+1), //y-coordinate
 
-						0.00001 //z-coordinate; place points just in front of gridlines, background
+						zDist+0.00001 //z-coordinate - place points just in front of the grid and background
 					));
 
 					k++;
 				}
 
-				cloud[t].position.copy(plane2.position);
 				camera.add(cloud[t]);
 			}
 		}
